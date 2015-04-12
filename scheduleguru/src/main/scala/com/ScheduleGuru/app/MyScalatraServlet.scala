883d25da770a918ce04d5f75d25fbd1d2adcaba9
Lu <lliu@trinity.edu>
2015-04-11 21:25:46 -0500

    added aboutus.html to MyScalatraServelet.scala

--- scheduleguru/src/main/scala/com/ScheduleGuru/app/MyScalatraServlet.scala
+++ scheduleguru/src/main/scala/com/ScheduleGuru/app/MyScalatraServlet.scala
@@ -355,4 +355,118 @@ class MyScalatraServlet extends ScheduleguruStack {
 
 </html>
 }
+	
+	get("/aboutus.html"){
+<html>
+
+<head>
+	<title>Contact</title>
+	<meta charset="UTF-8"></meta>
+	<meta name="description" content=""></meta>
+	<meta name="keywords" content=""></meta>
+	 <link rel="stylesheet" type="text/css" href="./css/master.css"></link>
+</head>
+
+<body>
+	<center>
+		<h1> Contact Us</h1>
+	</center>
+
+				    <!--menu bar-->
+    	    <nav>
+                <ul>
+                    <li><a href ="welcome.html">Home</a></li>
+					<li><a href ="build1.html">Build Schedule</a></li>
+				    <li><a href="aboutus.html">About Us</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+
+	<section id="aboutus">
+		<p style="background:white; opacity:0.8;">
+			<center style="font-size: 20px; font-family:cursive, verdana;">
+				Our story begin many many days ago, when we were first assigned to this project. We took this journey in an attempt to improve our current model of TigerPaws -- which is a terrible, archiac way of scheduling classes.
+
+				<br>
+				<br> Meet the Team:
+				<br>
+
+
+				<table width="500px" height="100%" border="2" color=white>
+
+					<tr>
+						<td>
+							<img src="imgs/rb.jpeg" alt="Rob Bierman" />
+						</td>
+
+						<td>
+							Rob Bierman
+							<br>
+							<br>
+							Our fearless hobbit
+						</td>
+					</tr>
+
+
+					<tr>
+						<td>
+							<img src="imgs/kj.jpeg" alt="Kendrick James" />
+						</td>
+
+						<td>
+							Kendrick James
+							<br>
+							<br>
+							No description needed.
+						</td>
+					</tr>
+					
+					
+					<tr>
+						<td>
+							<img src="imgs/ll.jpeg" alt="Lu Liu" />
+						</td>
+
+						<td>
+							Lu Liu
+							<br>
+							<br>
+							Need to figure out how to crop pics
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<img src="imgs/co.jpeg" alt="Caleb Olson" />
+						</td>
+
+						<td>
+							Caleb Olson
+							<br>
+							<br>
+							Where's the Algorithm?!
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<img src="imgs/zs.jpeg" alt="Zach Solcher" />
+						</td>
+
+						<td>
+							Zach Solcher
+							<br>
+							<br>
+							Webmaster
+						</td>
+					</tr>
+				</table>
+			</center>
+		</p>
+	</section>
+
+</body>
+
+</html>
+	}
 }
