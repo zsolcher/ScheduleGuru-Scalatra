850a67e5941502a9902791099e6abf159d02c05c
2012-08-22 <kjames@dias11.cs.trinity.edu>
2015-04-17 14:26:05 -0500

    started splitting things

--- scheduleguru/src/main/scala/ScalatraBootstrap.scala
+++ scheduleguru/src/main/scala/ScalatraBootstrap.scala
@@ -1,15 +1,10 @@
-import org.scalatra._
-import org.slf4j.LoggerFactory
-import com.ScheduleGuru.app._
-import javax.servlet.ServletContext
-import slick.driver.JdbcDriver.api._
-import slick.driver.MySQLDriver.simple._
-import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-import java.sql.SQLException
-import java.sql.Statement;
+
+import org.scalatra.LifeCycle
+
+import com.ScheduleGuru.app._
+
+import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
 
@@ -22,7 +17,9 @@ class ScalatraBootstrap extends LifeCycle {
     val pswd = "0757477";
     connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/ScheduleGuru", username, pswd);
     //val db = Database.forURL("jdbc:mysql://localhost:3306/ScheduleGuru", "kjames", "0757477", "com.mysql.jdbc.Driver")
-    context.mount(new MyScalatraServlet(new MyDatabase(connect)), "/*")
+    val db = new MyDatabase(connect)
+    context.mount(new MyScalatraServlet(db), "/*")
+    context.mount(new MyLoginServlet(db), "/login")
 
   }
 
